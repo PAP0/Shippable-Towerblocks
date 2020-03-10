033d56423c7a31400a57c5cb493f34f9e50ff757
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-03-11 00:04:04 +0100

    # Shippable Tower Stacker  ### Unity: 2019.3.0f5  ### Uitleg: Er is een scoresysteem die er 1 bijtelt als een block spawnt Blocks kunnen met muis gespawnt worden Wanneer een block valt op de grond is er een game over scherm met het score Je kan het spel restarten door op de restart knop te drukken

---
+++ DIP 1a/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float newY = 27.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) == true)
+        {
+            newY += 6.325f;
+        }
+        
+        //transform.position = new Vector3(0,newY,-8);
+        Vector3 desiredPosition = new Vector3(0, newY, -157.76f);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, 1f * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Shippable Tower Stacker
+
+### Unity: 2019.3.0f5
+
+### Uitleg:
+Er is een scoresysteem die er 1 bijtelt als een block spawnt
+Blocks kunnen met muis gespawnt worden
+Wanneer een block valt op de grond is er een game over scherm met het score
+Je kan het spel restarten door op de restart knop te drukken
+
+
+
